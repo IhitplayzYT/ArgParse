ada446eee0dbaf49c07a2841d69ce2bd2c054aa6
Ihit Acharya <ihitexe@gmail.com>
2026-07-29 10:40:05 +0530

    Added some more features and functions

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,69 @@ pub mod Helper{
         flag_map: HashMap<String,(Vec<&'static str>,fn (&'static dyn Any) -> &'static dyn Any)>,
         parse_map: HashMap<&'static str,fn (&str) -> &'static dyn Any>,
         pub preproc: Option<fn (Vec<String>) -> Vec<String>>,
-        pub help_fxn: fn()
+        pub help_fxn: fn(),
+        pub iter_fxn: Option<fn()>,
+        pub postproc: Option<fn (&'static dyn Any) -> &'static dyn Any>,
+        pub endlogic: Option<fn()>,
     }
-
-
-    fn __DEFAULT_PREPROC(mut args: Vec<String>) -> Vec<String>{
-        args.remove(0);
-        args
-    }
-
-    fn __DEFAULT_HELP(){
-        eprintln!("Somethign went Wrong!");
-        exit(0);
-    }
+    
 
     impl CLI{
-        pub fn new() -> Self{
-            Self { args: HashMap::new(), flag_map: HashMap::new(), parse_map: HashMap::new(), preproc:Some(__DEFAULT_PREPROC),help_fxn:__DEFAULT_HELP}
+
+        fn __DEFAULT_PREPROC(mut args: Vec<String>) -> Vec<String>{
+            args.remove(0);
+            args
         }
 
+        fn __DEFAULT_HELP(){
+            eprintln!("Somethign went Wrong!");
+            exit(0);
+        }
+
+        pub fn new() -> Self{
+            Self { args: HashMap::new(), flag_map: HashMap::new(), parse_map: HashMap::new(), preproc:Some(Self::__DEFAULT_PREPROC),help_fxn:Self::__DEFAULT_HELP,iter_fxn:None,postproc:None,endlogic:None}
+        }
+
+        pub fn set_preproc(&mut self,fxn: fn (Vec<String>) -> Vec<String>){
+            self.preproc = Some(fxn);
+        }
+
+        pub fn set_postproc(&mut self,fxn: fn (&'static dyn Any) -> &'static dyn Any){
+            self.postproc = Some(fxn);
+        }
+
+        pub fn set_help(&mut self,fxn: fn ()){
+            self.help_fxn = fxn;
+        }
+
+        pub fn set_iter(&mut self,fxn: fn ()){
+            self.iter_fxn = Some(fxn);
+        }
+
+
+        pub fn add_flag(&mut self,arg_name:&'static str,flags:(Vec<&'static str>,fn(&'static dyn Any) -> &'static dyn Any),default_val: &'static dyn Any){
+            self.args.insert(arg_name.to_string(), default_val);
+            self.flag_map.insert(arg_name.to_string(),flags);
+        } 
+
+        pub fn add_params(&mut self,params: Vec<(&'static str,fn (&str) -> &'static dyn Any,&'static dyn Any)>) {
+            for i in &params{
+                self.args.insert(i.0.to_string(), i.2);
+                self.parse_map.insert(i.0,i.1);
+            }
+        } 
+
+        pub fn add_flags(&mut self,flags:Vec<(&'static str,(Vec<&'static str>,fn(&'static dyn Any) -> &'static dyn Any),&'static dyn Any)>){
+            for i in &flags{
+                self.args.insert(i.0.to_string(), i.2);
+                self.flag_map.insert(i.0.to_string(),i.1.clone());
+            }
+        } 
+
+        pub fn add_param(&mut self,arg_name:&'static str,cust_str_fxn: fn (&str) -> &'static dyn Any,default_val: &'static dyn Any) {
+            self.args.insert(arg_name.to_string(), default_val);
+            self.parse_map.insert(arg_name,cust_str_fxn);
+        } 
 
 
         pub fn add_arg(&mut self,arg_name:&'static str,flags:Option<(Vec<&'static str>,fn(&'static dyn Any) -> &'static dyn Any)>,default_val: &'static dyn Any,cust_str_fxn: Option<fn (&str) -> &'static dyn Any>){
@@ -37,6 +81,19 @@ pub mod Helper{
             }
         }
 
+        pub fn add_args(&mut self,args:Vec<(&'static str,Option<(Vec<&'static str>,fn(&'static dyn Any) -> &'static dyn Any)>,&'static dyn Any,Option<fn (&str) -> &'static dyn Any>)>){
+            for i in &args{
+                self.add_arg(i.0, i.1.clone(), i.2, i.3);
+            }
+        }
+
+        pub fn Parse_Args(args:Vec<(&'static str,Option<(Vec<&'static str>,fn(&'static dyn Any) -> &'static dyn Any)>,&'static dyn Any,Option<fn (&str) -> &'static dyn Any>)>) -> CLI{
+            let mut ret = CLI::new();
+            ret.add_args(args);
+            ret.Parse();
+            return ret;
+        }
+
         pub fn Parse(&mut self){
             let mut clargs = std::env::args().collect::<Vec<String>>();
             if let Some(x) = self.preproc{
@@ -46,7 +103,10 @@ pub mod Helper{
                 if self.flag_map.contains_key(i){
                     if let Some(p) = self.args.get_mut(i){
                         if let Some(V) = self.flag_map.get(i){
-                            *p = V.1(p.clone());
+                            *p = V.1(*p);
+                            if let Some(x) = self.iter_fxn{
+                                x();
+                            }
                         }
                     }
                 }else{
@@ -54,6 +114,9 @@ pub mod Helper{
                         if i.starts_with(k){
                             if let Some(p) = self.args.get_mut(i){
                                 *p = func(i);
+                                if let Some(x) = self.iter_fxn{
+                                    x();
+                                }
                             }       
 
                         }
@@ -62,6 +125,16 @@ pub mod Helper{
                 }
 
             }
+            if let Some(x) = self.postproc{
+                for (_,v) in &mut self.args{
+                    *v = x(*v);
+                }
+            }
+            if let Some(x) = self.endlogic{
+                x();
+            }
+
+
         }
 
         
